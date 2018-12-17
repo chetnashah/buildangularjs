aeef6c9eb117ac29e2a1bdf7f69fcd47a0758e01
Chetna Shah <chetshah80@gmail.com>
2018-12-18 00:13:33 +0530

    kljkljl

--- src/scope.js
+++ src/scope.js
@@ -7,19 +7,41 @@ function Scope(){
     // scope needs to store all watchers
     // $$ means private to angularjs framework
     this.$$watchers = [];
+    this.$$asyncQueue = [];
+    this.$$applyAsyncQueue = [];
+    this.$$applyAsyncId = null;
+    this.$$phase = '';
 }
 
-
-Scope.prototype.$watch = function(watchFn, listenerFn) {
+// listener function can modify scope values
+// listener function can also add another watchers using $watch
+// listenr function can modify scope in cyclic fashion to create deadlock
+Scope.prototype.$watch = function(watchFn, listenerFn, valueEq) {
     // watch registration happens here, add them to list of watchers
     // these functions are only called during digest cycle
     this.$$watchers.push({
         watchFn: watchFn,
         listenerFn: listenerFn || function(){},
+        valueEq: !!valueEq,
         last: initWatchVal
     });
+    // lastdirtywatch stores last dirty watcher in the list  at a scope level
+    // also reset this scope level variable when any new watcher is added.
+    this.$$lastDirtyWatch = null;
 }
 
+Scope.prototype.$$areEqual = function(newVal, oldVal, valueEq) {
+    // simple isNaN(undefined) is true
+    // we want to explicitly check for NaN so we use Number.isNaN
+    if(Number.isNaN(newVal) && Number.isNaN(oldVal)){
+        return true;
+    }
+    if(valueEq) {
+        return _.isEqual(newVal, oldVal);
+    } else {
+        return newVal === oldVal;
+    }
+}
 
 // runs all watchers once and
 // returns a boolean telling if something was dirty
@@ -29,15 +51,19 @@ Scope.prototype.$$digestOnce = function(){
     var oldVal, newVal, dirty;
     // every registered watch function is called in digest cycle
     _.forEach(this.$$watchers, function(watcher){
+        
         oldVal = watcher.last;// we remember last value in the watcher
         newVal = watcher.watchFn(self);
         // console.log(newVal, oldVal);
-        if(newVal != oldVal){
-            watcher.last = newVal;
-            dirty = true;
+        if(!self.$$areEqual(newVal, oldVal, watcher.valueEq)){
+            self.$$lastDirtyWatch = watcher;
+            watcher.last = watcher.valueEq ? _.cloneDeep(newVal) : newVal;
             // when going from initVal to any other val
             // return same for both newVal and oldVal
             watcher.listenerFn(newVal, oldVal === initWatchVal ? newVal : oldVal, self);
+            dirty = true;
+        } else if (self.$$lastDirtyWatch === watcher) {
+            return false;
         }
     });
     return dirty;
@@ -46,13 +72,105 @@ Scope.prototype.$$digestOnce = function(){
 // runs at least once - all the watchers
 // keep digesting while dirty
 Scope.prototype.$digest = function(){
+    this.$beginPhase('$digest');// digest phase begins
     var ttl = 10;
+    debugger;
+    this.$$lastDirtyWatch = null;
+
+    // flush & clear applyAsync functions if present
+    if (this.$$applyAsyncId) {
+        clearTimeout(this.$$applyAsyncId);
+        this.$$flushApplyAsync();
+    }
+
    var dirty = this.$$digestOnce();
-   while(dirty && ttl--){
-       dirty = this.$$digestOnce();
+   console.trace('outside');
+   console.log('outside dirty = ', dirty, this);
+   while((dirty && ttl--) || (this.$$asyncQueue.length && ttl--)){
+      while(this.$$asyncQueue.length) {
+        var asyncTask = this.$$asyncQueue.shift();
+        asyncTask.scope.$eval(asyncTask.expression);
+      }
+      dirty = this.$$digestOnce();
+      console.log('inside dirty = ', dirty, this);
    }
+   this.$clearPhase();
    if (ttl <= 0) {
        throw new Error('Reached TTL limit');
    }
 
+}
+
+/**
+ * $apply: ((scope) => any) => any
+ */
+Scope.prototype.$apply = function(expr) {
+    var result;
+    try {
+        this.$beginPhase('$apply'); // apply phase begins
+        result = this.$eval(expr);
+    } catch(err) {
+        console.err('some error happened : ', err);
+    } finally {
+        this.$clearPhase(); // apply phase ends
+        this.$digest(); // run digest phase after apply phase
+        return result;
+    }
+}
+
+// call all functions in applyAsyncQueue
+Scope.prototype.$$flushApplyAsync = function(){
+    while(this.$$applyAsyncQueue.length) {
+        this.$$applyAsyncQueue.shift()();
+    }
+    this.$$applyAsyncId = null;
+}
+
+Scope.prototype.$applyAsync = function(expr) {
+    var self = this;
+    // defering by putting eval-expr in func
+    self.$$applyAsyncQueue.push(function(){
+        self.$eval(expr);
+    });
+    // we are not calling digest
+    // we are letting all digest finish and run all jobs in a single $apply
+    if (!this.$$applyAsyncId) {
+        this.$$applyAsyncId = setTimeout(function(){
+            self.$apply(_.bind(self.$$flushApplyAsync, self))
+        }, 0); 
+    }
+}
+
+//this triggers a $digest if not already present
+Scope.prototype.$evalAsync = function(expr) {
+    var self = this;
+    // check if no phase is running and no task pending b4 adding task
+    if(!self.$$phase && !self.$$asyncQueue.length) {
+        setTimeout(function(){
+            if (self.$$asyncQueue.length) {
+                self.$digest();
+            }
+        }, 0);
+    }
+    this.$$asyncQueue.push({scope: this, expression: expr});
+}
+
+/**
+ * $eval: ((scope) => any, locals) => any
+ */
+Scope.prototype.$eval = function(expr, locals) {
+    return expr(this, locals);
+}
+
+// we have a begin phase and a clear phase
+Scope.prototype.$beginPhase = function(phase){
+    if(this.$$phase) {
+        throw this.$$phase + ' already in progress';
+    }
+    this.$$phase = phase;
+}
+
+// set this.$$phase to null
+Scope.prototype.$clearPhase = function(phase) {
+    this.$$phase = null;
 }
--- test/scope__spec.js
+++ test/scope__spec.js
@@ -147,6 +147,309 @@ describe('Scope', function(){
 
             expect((function(){ scope.$digest(); })).toThrow();
         });
+        
+        it('short circuits on a clean watch', function(){
+            scope.array = _.range(100);
+            var watchExecutions = 0;
 
+            // setup watcher for each element in array
+            _.times(100, function(i){
+                scope.$watch(
+                    function(sc){ watchExecutions++; return sc.array[i];},
+                    function(newVal, oldVal, sc){}
+                );
+            });
+
+            scope.$digest();
+            expect(watchExecutions).toBe(200);
+
+            scope.array[0] = 1;
+            scope.$digest();
+            expect(watchExecutions).toBe(301);
+
+        });
+
+        it('does not end digest, and lets new watchers e.g. added in listeners also run', function(){
+            scope.someValue = 'a';
+            scope.ctr = 0;
+
+            scope.$watch(
+                function(sc) { return sc.someValue; },
+                function(newVal, oldVal, sc) {
+                    console.log('oldVal = ', oldVal);
+                    console.log('newVal = ', newVal);
+                    scope.$watch(
+                        function(sc2) { return sc2.someValue; },
+                        function(newVal2, oldVal2, sc2){
+                            sc2.ctr++;
+                        }
+                    )
+                }
+            );
+
+            scope.$digest();
+            expect(scope.ctr).toBe(1);
+            
+        });
+
+        it(' supports value equality watching instead of references', function(){
+            scope.arr = [1,2,3];
+            scope.ctr = 0;
+            scope.$watch(
+                function(sc) { return sc.arr; },
+                function(newVal, oldVal, sc) {
+                    console.log('arr changed, arr = ', newVal);
+                    scope.ctr++;
+                },
+                true
+            );
+            scope.$digest();
+            expect(scope.ctr).toBe(1);
+
+            scope.arr[2] = -1;
+            scope.$digest();
+            expect(scope.ctr).toBe(2);
+
+            
+        });
+
+        it('correctly handles NaNs', function(){
+            scope.n = 0/0;// NaN
+            scope.ctr = 0;
+            scope.$watch(
+                function(sc) { return sc.n; },
+                function(newVal, oldVal, sc){
+                    sc.ctr++;
+                }
+            );
+
+            scope.$digest();
+            expect(scope.ctr).toBe(1);
+
+            scope.$digest();
+            expect(scope.ctr).toBe(1);
+        });
     });
-})
+
+    describe('$eval', function(){
+        var scope;
+        beforeEach(function(){
+            scope = new Scope();
+        });
+
+        it('executes evaled function and returns the result', function(){
+            scope.a = 42;
+            var result = scope.$eval(
+                function(sc){
+                    return sc.a;
+                }
+            );
+
+            expect(result).toBe(42);
+
+            // should pass the locals as is
+            var res2 = scope.$eval(
+                function(sc, locals) { return sc.a + locals; },
+                8
+            );
+
+            expect(res2).toBe(50);
+        });
+
+        it('executes apply\'ed function and runs digest', function(){
+            scope.val = 'aval';
+            scope.ctr = 0;
+
+            scope.$watch(
+                function(sc) { return sc.val; },
+                function(newVal, oldVal, sc) { sc.ctr++; }
+            );
+
+            scope.$digest();
+            expect(scope.ctr).toBe(1);
+
+            scope.$apply(function(sc){
+                sc.val = 'bval';
+            });
+            expect(scope.ctr).toBe(2);
+        });
+
+        it("executes $evalAsync'ed code later in the same digest", function(){
+            scope.aval = 'a';
+            scope.asyncEvaluated = false;
+            scope.asyncEvaluatedImmediately = false;
+
+            scope.$watch(
+                function(sc) { return sc.aval; },
+                function(newVal, oldVal, sc) {
+                    sc.$evalAsync(function(sc){
+                        sc.asyncEvaluated = true;
+                    });
+                    sc.asyncEvaluatedImmediately = sc.asyncEvaluated;
+                }
+            );
+
+            scope.$digest();
+            expect(scope.asyncEvaluated).toBe(true);
+            expect(scope.asyncEvaluatedImmediately).toBe(false);
+        });
+
+        it('eventually halts tasks added by evalAsync in watch functions', function(){
+            scope.aval = 'a';
+           
+            scope.$watch(
+                function(sc){
+                    sc.$evalAsync(function(sc){ });
+                    return sc.aval;
+                },
+                function(newVal, oldVal, sc) {}
+            );
+
+            expect(function(){ scope.$digest() }).toThrow();
+        });
+
+        it('has a $$phase field whose value is the current digest phase', function(){
+            scope.avalue = 'a';
+            scope.phaseInWatchFunction = undefined;
+            scope.phaseInListenerFunction = undefined;
+            scope.phaseInApplyFunction = undefined;
+
+            scope.$watch(
+                function(sc){
+                    sc.phaseInWatchFunction = sc.$$phase;
+                    return sc.avalue;
+                },
+                function(newVal, oldVal, sc) {
+                    scope.phaseInListenerFunction = sc.$$phase;
+                }
+            );
+
+            scope.$apply(function(sc){
+                scope.phaseInApplyFunction = sc.$$phase;
+            });
+
+            expect(scope.phaseInWatchFunction).toBe('$digest');
+            expect(scope.phaseInListenerFunction).toBe('$digest');
+            expect(scope.phaseInApplyFunction).toBe('$apply');
+
+        });
+
+        it('shedules a digest when $evalAsync is used', function(done){
+            scope.a = 'a';
+            scope.ctr = 0;
+
+            scope.$watch(
+                function(sc){ return sc.a; },
+                function(newVal, oldVal, sc){
+                    sc.ctr++;
+                }
+            );
+
+            scope.$evalAsync(function(){ });
+
+            expect(scope.ctr).toBe(0);
+            setTimeout(function(){
+                expect(scope.ctr).toBe(1);
+                done();
+            }, 50);
+        });
+
+        it('allows async $apply via $applyAsync', function(done){
+            scope.aval = 'a';
+            scope.ctr = 0;
+
+            scope.$watch(
+                function(sc) { return scope.aval; },
+                function(newVal, oldVal, sc) {
+                    sc.ctr++;
+                }
+            );
+
+            scope.$digest();
+            expect(scope.ctr).toBe(1);
+
+            scope.$applyAsync(function(sc){
+                sc.aval = 'b';
+            });
+            expect(scope.ctr).toBe(1);
+            setTimeout(function(){
+                expect(scope.ctr).toBe(2);
+                done();
+            },50);
+        });
+
+        it("never executes $applyAsync'ed function in the same digest cycle where it used", function(done){
+            scope.aval = 'a';
+            scope.asyncApplied = false;
+
+            scope.$watch(
+                function(sc) { return sc.aval; },
+                function(newVal, oldVal, sc) {
+                    scope.$applyAsync(function(sc){// applyAsync schedules its own apply via timeout
+                        sc.asyncApplied = true;
+                    });
+                }
+            );
+
+            scope.$digest();
+            // no change after digest, apply will run it's own digest & process tasks
+            expect(scope.asyncApplied).toBe(false);
+            setTimeout(function(){
+                expect(scope.asyncApplied).toBe(true);
+                done();
+            }, 50);
+        });
+
+        it('coalasces many calls to $applyAsync', function(done){
+            scope.watchCtr = 0;
+            scope.listenCtr = 0;
+
+            scope.$watch(
+                function(sc) { sc.watchCtr++; return sc.aval; },
+                function(newVal, oldVal, sc) { sc.listenCtr++; }
+            );
+
+            scope.$applyAsync(function(sc){
+                sc.aval = 'abc';
+            });
+            scope.$applyAsync(function(sc){
+                sc.aval = 'def';
+            });
+
+            setTimeout(function(){
+                expect(scope.watchCtr).toBe(2);
+                expect(scope.listenCtr).toBe(1);
+                done();
+            }, 20);
+        });
+
+        it('flushes applyAsync calls(execute synchronously) if digest is run', function(done){
+            scope.watchCtr =0;
+
+            scope.$watch(
+                function(sc) {
+                    sc.watchCtr++;
+                    return sc.aVal;
+                },
+                function(newVal, oldVal, sc) { }
+            );
+
+            scope.$applyAsync(function(sc){
+                sc.aVal="abc";
+            });
+            scope.$applyAsync(function(sc){
+                sc.aVal = 'def';
+            });
+
+            scope.$digest();
+            expect(scope.watchCtr).toBe(2);
+            expect(scope.aVal).toBe('def');
+
+            setTimeout(function(){
+                expect(scope.watchCtr).toBe(2);
+                done();
+            }, 50);
+        });
+
+    })
+});
